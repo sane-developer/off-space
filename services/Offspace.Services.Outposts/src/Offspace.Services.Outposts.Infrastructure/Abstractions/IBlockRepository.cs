7b37a69b9aa8309d8c81bd74b70e00ff80747b8b
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-19 00:08:46 +0200

    [1.0.0.5] Primitive repository for block table.

---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Abstractions/IBlockRepository.cs
@@ -0,0 +1,36 @@
+﻿using Offspace.Services.Outposts.Domain.Entities;
+
+namespace Offspace.Services.Outposts.Infrastructure.Abstractions;
+
+/// <summary>
+///     Represents a set of methods required for interacting with the <see cref="Block"/> entity table.
+/// </summary>
+public interface IBlockRepository
+{
+    /// <summary>
+    ///     Gets a <see cref="Block"/> entity by its id.
+    /// </summary>
+    /// <param name="blockId">The id of the requested block entity.</param>
+    /// <returns><i>The requested block entity</i> if it has been found in the database; else <i>null</i>.</returns>
+    ValueTask<Block?> GetBlockAsync(int blockId);
+    
+    /// <summary>
+    ///     Updates the <see cref="Block.OutpostId"/> property of a <see cref="Block"/> entity.
+    /// </summary>
+    /// <param name="block">The block entity to be updated.</param>
+    /// <param name="newOutpostId">The id of the outpost the block will be moved to.</param>
+    /// <returns>
+    ///     TODO: Fill this once decided how to handle the return value.
+    /// </returns>
+    Task<bool> UpdateBlockOutpostAsync(Block block, int? newOutpostId);
+    
+    /// <summary>
+    ///     Updates the <see cref="Block.Position"/> property of a <see cref="Block"/> entity.
+    /// </summary>
+    /// <param name="block">The block entity to be updated.</param>
+    /// <param name="newPosition">The new position of the block within the outpost.</param>
+    /// <returns>
+    ///     TODO: Fill this once decided how to handle the return value.
+    /// </returns>
+    Task<bool> UpdateBlockPositionAsync(Block block, int newPosition);
+}
---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Repositories/BlockRepository.cs
@@ -0,0 +1,51 @@
+﻿using Offspace.Services.Outposts.Domain.Entities;
+using Offspace.Services.Outposts.Infrastructure.Abstractions;
+using Offspace.Services.Outposts.Infrastructure.Contexts;
+
+namespace Offspace.Services.Outposts.Infrastructure.Repositories;
+
+/// <summary>
+///     Represents a repository responsible for interacting with the <see cref="Block"/> entity table.
+/// </summary>
+public sealed class BlockRepository : IBlockRepository
+{
+    /// <summary>
+    ///     The database context for interacting with the <see cref="Block"/> entity table.
+    /// </summary>
+    private readonly BlockDbContext _context;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="BlockRepository"/> class with the specified database context.
+    /// </summary>
+    public BlockRepository(BlockDbContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask<Block?> GetBlockAsync(int blockId)
+    {
+        return await _context.Blocks.FindAsync(blockId);
+    }
+    
+    public async Task<bool> UpdateBlockOutpostAsync(Block block, int? newOutpostId)
+    {
+        block.OutpostId = newOutpostId;
+        
+        _context.Blocks.Update(block);
+
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+    
+    public async Task<bool> UpdateBlockPositionAsync(Block block, int newPosition)
+    {
+        block.Position = newPosition;
+        
+        _context.Blocks.Update(block);
+
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+}
