710bcc4e0c5e9c90b21192c6f5d13a5511eaf7f0
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-18 23:05:35 +0200

    [1.0.0.3] Defined request and response structures.

--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Data/block.sql
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Data/block.sql
@@ -3,14 +3,14 @@ create table block
     id         INTEGER not null
         constraint block_pk
             primary key autoincrement,
-    type       TEXT    not null,
-    position   INT     not null,
-    outpost_id INTEGER
+    type       TEXT    default NULL,
+    position   INT     default NULL,
+    outpost_id INTEGER default NULL
         constraint outpost_fk
             references outpost,
     constraint is_valid_position
-        check (position >= 0 AND position <= 48),
+        check ((position >= 0 AND position <= 48) OR NULL),
     constraint is_valid_type
-        check (type IN ('regular', 'root'))
+        check (type IN ('regular', 'root') OR NULL)
 );
 
--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Domain/Entities/Block.cs
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Domain/Entities/Block.cs
@@ -13,20 +13,20 @@ public class Block
     /// <summary>
     ///     The type of the block.
     /// </summary>
-    public string Type { get; set; } = default!;
+    public string? Type { get; set; }
     
     /// <summary>
     ///     The position of the block in the outpost grid.
     /// </summary>
-    public int Position { get; set; }
+    public int? Position { get; set; }
     
     /// <summary>
     ///     The id of the outpost the block belongs to.
     /// </summary>
-    public int OutpostId { get; set; }
+    public int? OutpostId { get; set; }
     
     /// <summary>
     ///     The outpost the block belongs to.
     /// </summary>
-    public Outpost Outpost { get; set; } = default!;
+    public Outpost? Outpost { get; set; }
 }
