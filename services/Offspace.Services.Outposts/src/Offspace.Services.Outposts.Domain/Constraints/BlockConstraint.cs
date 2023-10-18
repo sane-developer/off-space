e685361f45f578398fcd976ab20a9fbcb4539b01
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-18 16:22:44 +0200

    [1.0.0.2] Defined constraints for database entities.
    Additionally, restricted .NET version.

---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Domain/Constraints/BlockConstraint.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offspace.Services.Outposts.Domain.Abstractions;
+using Offspace.Services.Outposts.Domain.Entities;
+
+namespace Offspace.Services.Outposts.Domain.Constraints;
+
+/// <summary>
+///     Represents a set of constraints for the <see cref="Block"/> entity.
+/// </summary>
+public sealed class BlockConstraint : IEntityConstraint
+{
+    private const string TableName = "block";
+    
+    private const string IdColumnName = "id";
+    
+    private const string TypeColumnName = "type";
+    
+    private const string PositionColumnName = "position";
+    
+    private const string OutpostIdColumnName = "outpost_id";
+    
+    public static void Apply(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Block>()
+            .ToTable(TableName);
+        
+        modelBuilder
+            .Entity<Block>()
+            .Property(block => block.Id)
+            .HasColumnName(IdColumnName)
+            .HasColumnType("INTEGER")
+            .IsRequired();
+        
+        modelBuilder
+            .Entity<Block>()
+            .Property(block => block.Type)
+            .HasColumnName(TypeColumnName)
+            .HasColumnType("TEXT")
+            .IsRequired();
+
+        modelBuilder
+            .Entity<Block>()
+            .Property(block => block.Position)
+            .HasColumnName(PositionColumnName)
+            .HasColumnType("INT")
+            .IsRequired();
+
+        modelBuilder
+            .Entity<Block>()
+            .Property(block => block.OutpostId)
+            .HasColumnName(OutpostIdColumnName)
+            .HasColumnType("INTEGER")
+            .IsRequired(false);
+        
+        modelBuilder
+            .Entity<Block>()
+            .HasKey(block => block.Id);
+
+        modelBuilder
+            .Entity<Block>()
+            .HasOne(block => block.Outpost)
+            .WithMany(outpost => outpost.Blocks)
+            .HasForeignKey(block => block.OutpostId);
+    }
+}
---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Domain/Constraints/OutpostConstraint.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offspace.Services.Outposts.Domain.Abstractions;
+using Offspace.Services.Outposts.Domain.Entities;
+
+namespace Offspace.Services.Outposts.Domain.Constraints;
+
+/// <summary>
+///     Represents a set of constraints for the <see cref="Outpost"/> entity.
+/// </summary>
+public sealed class OutpostConstraint : IEntityConstraint
+{
+    private const string TableName = "outpost";
+    
+    private const string IdColumnName = "id";
+    
+    private const string NameColumnName = "name";
+    
+    public static void Apply(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Outpost>()
+            .ToTable(TableName);
+        
+        modelBuilder
+            .Entity<Outpost>()
+            .Property(outpost => outpost.Id)
+            .HasColumnName(IdColumnName)
+            .HasColumnType("INTEGER")
+            .IsRequired();
+        
+        modelBuilder
+            .Entity<Outpost>()
+            .Property(outpost => outpost.Name)
+            .HasColumnName(NameColumnName)
+            .HasColumnType("TEXT")
+            .IsRequired();
+        
+        modelBuilder
+            .Entity<Outpost>()
+            .HasKey(outpost => outpost.Id);
+        
+        modelBuilder
+            .Entity<Outpost>()
+            .HasMany(outpost => outpost.Blocks)
+            .WithOne(block => block.Outpost)
+            .HasForeignKey(block => block.OutpostId)
+            .IsRequired(false);
+        
+        modelBuilder
+            .Entity<Outpost>()
+            .HasIndex(outpost => outpost.Name)
+            .IsUnique();
+    }
+}
