c00ed32de8f1284759a2e455ba8839a91e7e26cb
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-22 20:54:02 +0200

    [1.0.1.5] Definitions for outpost service & repository.

--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Abstractions/IOutpostService.cs
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Abstractions/IOutpostService.cs
@@ -1,6 +1,4 @@
-﻿using Offspace.Services.Outposts.Domain.Entities;
-
-namespace Offspace.Services.Outposts.Infrastructure.Abstractions;
+﻿namespace Offspace.Services.Outposts.Infrastructure.Abstractions;
 
 /// <summary>
 ///     Represents a contract for a service which enables the user to manipulate the state of the outposts.
