e2efdb17ee940b5a5fed8b77f9c0378442e7e850
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-22 20:57:45 +0200

    Outpost service implements its interface now.

--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Services/OutpostService.cs
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Services/OutpostService.cs
@@ -1,9 +1,12 @@
-﻿namespace Offspace.Services.Outposts.Infrastructure.Services;
+﻿using Offspace.Services.Outposts.Domain.Entities;
+using Offspace.Services.Outposts.Infrastructure.Abstractions;
+
+namespace Offspace.Services.Outposts.Infrastructure.Services;
 
 /// <summary>
-///     Represents a contract for a service which enables the user to manipulate the state of the outposts.
+///     Represents a contract for a service which enables the user to manipulate the state of the <see cref="Outpost"/> entities.
 /// </summary>
-public sealed class OutpostService
+public sealed class OutpostService : IOutpostService
 {
     
 }
