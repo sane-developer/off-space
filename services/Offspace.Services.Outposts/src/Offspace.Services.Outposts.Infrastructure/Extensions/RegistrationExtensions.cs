993796fd5c50e1450262d8d1e2b710bf6472c094
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-22 21:12:48 +0200

    [1.0.1.6] Dependencies registration extension methods.

---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.Infrastructure/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Offspace.Services.Outposts.Infrastructure.Abstractions;
+using Offspace.Services.Outposts.Infrastructure.Contexts;
+using Offspace.Services.Outposts.Infrastructure.Repositories;
+using Offspace.Services.Outposts.Infrastructure.Services;
+
+namespace Offspace.Services.Outposts.Infrastructure.Extensions;
+
+/// <summary>
+///     Represents a set of extension methods responsible for registering the infrastructure services.
+/// </summary>
+public static class RegistrationExtensions
+{
+    /// <summary>
+    ///     Registers the database contexts used by the infrastructure layer.
+    /// </summary>
+    public static void AddDatabaseContexts(this IServiceCollection services)
+    {
+        services.AddDbContext<BlockDatabaseContext>();
+        services.AddDbContext<OutpostDatabaseContext>();
+    }
+
+    /// <summary>
+    ///     Registers the repositories used by the infrastructure layer.
+    /// </summary>
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddSingleton<IBlockRepository, BlockRepository>();
+        services.AddSingleton<IOutpostRepository, OutpostRepository>();
+    }
+
+    /// <summary>
+    ///     Registers the services used by the infrastructure layer.
+    /// </summary>
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IBlockService, BlockService>();
+        services.AddSingleton<IOutpostService, OutpostService>();
+    }
+}
