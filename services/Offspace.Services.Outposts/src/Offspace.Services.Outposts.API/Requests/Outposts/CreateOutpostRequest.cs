fb09e4cdaf8ebe6039b6a62cd234b6ceb053d8dc
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-23 16:44:31 +0200

    [1.0.2.5] Request models for outpost endpoints.
    Additionally, removed the remarks on the block endpoints. I kinda feel like it should be accessible within one certain document accessible for everyone instead.

---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Requests/Outposts/CreateOutpostRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Offspace.Services.Outposts.API.Requests.Outposts;
+
+/// <summary>
+///     Represents a request to create an outpost.
+/// </summary>
+public record CreateOutpostRequest
+{
+    /// <summary>
+    ///     The name of the outpost.
+    /// </summary>
+    [FromQueryParams]
+    [BindFrom("name")]
+    public required string Name { get; init; } = default!;
+}
