47a5719070fb2911a87d3facf7a9357824151fae
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-23 15:51:15 +0200

    [1.0.2.3] Block detachment endpoint.

--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Endpoints/Blocks/DetachBlockEndpoint.cs
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Endpoints/Blocks/DetachBlockEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using Offspace.Services.Outposts.API.Requests.Blocks;
+using Offspace.Services.Outposts.Infrastructure.Services;
 
 namespace Offspace.Services.Outposts.API.Endpoints.Blocks;
 
@@ -8,5 +9,53 @@ namespace Offspace.Services.Outposts.API.Endpoints.Blocks;
 [HttpPatch("/api/blocks/{blockId:int}/detach")]
 public sealed class DetachBlockEndpoint : Endpoint<DetachBlockRequest>
 {
-    
+    /// <summary>
+    ///     The service which enables the user to manipulate the state of the verven blocks.
+    /// </summary>
+    private readonly BlockService _blockService;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="DetachBlockEndpoint"/> class with the specified services.
+    /// </summary>
+    public DetachBlockEndpoint(BlockService blockService)
+    {
+        _blockService = blockService;
+    }
+
+    /// <summary>
+    ///     Validates whether the block with the specified specification can be detached from the requested outpost.
+    /// </summary>
+    /// <remarks>
+    ///     The block can only be detached from the requested outpost when all of the conditions have been met:
+    ///     <list type="bullet">
+    ///         <item>The block with requested id exists,</item>
+    ///         <item>The block is attached to an outpost.</item>
+    ///     </list>
+    /// </remarks>
+    public override async Task HandleAsync(DetachBlockRequest req, CancellationToken ct)
+    {
+        var block = await _blockService.GetBlockAsync(req.BlockId);
+        
+        if (block is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+        
+        if (block.OutpostId is null)
+        {
+            await SendErrorsAsync(StatusCodes.Status400BadRequest, ct);
+            return;
+        }
+        
+        var hasDetached = await _blockService.DetachBlockFromOutpostAsync(block);
+        
+        if (!hasDetached)
+        {
+            await SendErrorsAsync(StatusCodes.Status500InternalServerError, ct);
+            return;
+        }
+        
+        await SendNoContentAsync(ct);
+    }
 }
