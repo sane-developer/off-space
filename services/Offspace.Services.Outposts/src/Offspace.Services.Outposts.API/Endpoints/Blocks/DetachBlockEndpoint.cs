cf625090ffe1debaffda6e36c9360113868cf03d
Krzysztof Wodziński <krzysztof.wodzinski.me@gmail.com>
2023-10-27 14:58:17 +0200

    [1.0.3.2] Major block detachment improvements.

--- services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Endpoints/Blocks/DetachBlockEndpoint.cs
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Endpoints/Blocks/DetachBlockEndpoint.cs
@@ -1,13 +1,23 @@
-﻿using Offspace.Services.Outposts.API.Requests.Blocks;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Offspace.Services.Outposts.API.Requests.Blocks;
+using Offspace.Services.Outposts.API.Responses;
+using Offspace.Services.Outposts.API.Responses.Blocks;
 using Offspace.Services.Outposts.Infrastructure.Abstractions;
 
 namespace Offspace.Services.Outposts.API.Endpoints.Blocks;
+
+using Responses = Results<
+    NoContent,
+    NotFound<Response>,
+    BadRequest<Response>,
+    StatusCodeHttpResult
+>;
 
 /// <summary>
 ///     Represents an endpoint that enables the user to detach a block from the requested outpost.
 /// </summary>
 [HttpPatch("/api/blocks/{blockId:int}/detach")]
-public sealed class DetachBlockEndpoint : Endpoint<DetachBlockRequest>
+public sealed class DetachBlockEndpoint : Endpoint<DetachBlockRequest, Responses>
 {
     /// <summary>
     ///     The service which enables the user to manipulate the state of the verven blocks.
@@ -25,30 +35,27 @@ public sealed class DetachBlockEndpoint : Endpoint<DetachBlockRequest>
     /// <summary>
     ///     Validates whether the block with the requested specification can be detached from the outpost.
     /// </summary>
-    public override async Task HandleAsync(DetachBlockRequest req, CancellationToken ct)
+    public override async Task<Responses> ExecuteAsync(DetachBlockRequest req, CancellationToken ct)
     {
         var block = await _blockService.GetBlockAsync(req.BlockId);
         
         if (block is null)
         {
-            await SendNotFoundAsync(ct);
-            return;
+            return TypedResults.NotFound<Response>(BlockNotFoundResponse.Instance);
         }
         
         if (block.OutpostId is null)
         {
-            await SendErrorsAsync(StatusCodes.Status400BadRequest, ct);
-            return;
+            return TypedResults.BadRequest<Response>(BlockNotAttachedResponse.Instance);
         }
         
         var hasDetached = await _blockService.DetachBlockFromOutpostAsync(block);
         
         if (!hasDetached)
         {
-            await SendErrorsAsync(StatusCodes.Status500InternalServerError, ct);
-            return;
+            return TypedResults.StatusCode(StatusCodes.Status500InternalServerError);
         }
-        
-        await SendNoContentAsync(ct);
+
+        return TypedResults.NoContent();
     }
 }
---
+++ services/Offspace.Services.Outposts/src/Offspace.Services.Outposts.API/Responses/Blocks/BlockNotAttachedResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Offspace.Services.Outposts.API.Responses.Blocks;
+
+/// <summary>
+///     Represents a response which indicates that the block is not attached to any outpost.
+/// </summary>
+public sealed record BlockNotAttachedResponse : Response
+{
+    /// <summary>
+    ///     The singleton instance of the response.
+    /// </summary>
+    public static readonly BlockNotAttachedResponse Instance = new();
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="BlockNotAttachedResponse"/> class with desired message.
+    /// </summary>
+    private BlockNotAttachedResponse()
+    {
+        Message = "The block is not attached to any outpost.";
+    }
+}
